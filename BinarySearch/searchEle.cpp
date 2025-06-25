abff69a3c9c4e59dfc300a8327525dc4dc83f1bc
Priya Sirohi <priyasirohi502@gmail.com>
2025-06-25 23:10:13 +0530

    Binary Search

---
+++ BinarySearch/searchEle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(int arr, int low, int high, int target){
+    while(low<=higj){
+    int mid = (low + high)/2;
+    
+    else if(mid == target) return mid;
+    else if (mid > target){
+        high = mid -1;
+    }
+    else{
+        low = mid+1;
+    }
+}
+return -1
+}
